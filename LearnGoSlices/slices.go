f26b2da2301cf4fd2d4039efc7f4865f40a121b4
Rafael Hernamperez <rafinguer@gmail.com>
2022-09-19 15:25:35 +0200

    More slices features

--- LearnGoSlices/slices.go
+++ LearnGoSlices/slices.go
@@ -2,15 +2,46 @@ package main
 
 import "fmt"
 
-func main() {
-	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+// Reference documentation:
+// https://blog.golang.org/go-slices-usage-and-internals
 
-	slice1 := list[3:7] // 4, 5, 6, 7
+func main() {
+
+	// Declaring slice without memory allocation
+	// var slice []int
+
+	// Declaring slice with memory allocation without values
+	// slice := []int{}
+
+	// Declaring slice with memory allocation and reserve length a capacity without values
+	// slice := make([]int, 5, 10)
+	// len(slice)   => 5
+	// cap(slice)   => 10
+
+	// Declaring slice with values
+	// slice := []int{1, 2, 3}
+
+	// Creating slice 'mySlice' with preinitialized values
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+
+	fmt.Println("Lenght of mySlice:", len(mySlice))   // 12
+	fmt.Println("Capacity of mySlice:", cap(mySlice)) // 12
+
+	// Accessing to a slice:
+	//    slice[lowIndex:highIndex]  // from low to high-1
+	//    slice[lowIndex:]           // from low to end
+	//    slice[:highIndex]          // from beginning to high-1
+	slice1 := mySlice[3:7] // 4, 5, 6, 7
 	fmt.Println("slice1: ", slice1)
 
-	slice2 := list[:7] // 1, 2, 3, 4, 5, 6, 7
+	slice2 := mySlice[:7] // 1, 2, 3, 4, 5, 6, 7
 	fmt.Println("slice2: ", slice2)
 
-	slice3 := list[3:] // 4, 5, 6, 7, 8, 9, 10, 11, 12
+	slice3 := mySlice[3:] // 4, 5, 6, 7, 8, 9, 10, 11, 12
 	fmt.Println("slice3: ", slice3)
+
+	// Operations with slices
+	slice1 = append(slice1, 20, 30) // Adding at the end => // 4, 5, 6, 7, 20, 30
+	fmt.Println("slice1: ", slice1)
+
 }
