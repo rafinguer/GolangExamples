8bca210ef1ad0b45bd8cda2ab30f42a3d4378dec
Rafael Hernamperez <rafinguer@gmail.com>
2022-09-20 21:18:17 +0200

    Added singleton patter in Hydra app implementing a logger

--- Hydra/webserver.go
+++ Hydra/webserver.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"Hydra/hlogger"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	logger := hlogger.GetInstance()
+	logger.Println("Starting Hydra Web Service...")
+
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8080", nil)
 }
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := hlogger.GetInstance()
 	fmt.Fprintf(w, "Welcome to the Hydra Webserver")
+	logger.Println("Received an http Get request on root url")
 }
