9fcbeeffd5671d2e68c663d332839e191ef0b128
Rafael Hernamperez <rafinguer@gmail.com>
2022-09-22 13:09:32 +0200

    Added unit testing template

---
+++ LearnGoUnitTesting/unittesting_test.go
@@ -0,0 +1,16 @@
+package main
+
+// Go Unit tests
+//    - Create files appended with "_test.go" for testing purposes
+//    - Import "testing" package
+//    - Write functions with the signature TextXxxx(*test.T)
+//    - Use "go test" command to test
+
+//func TestRun(t *Testing.T) {
+// TO DO code to execute tests
+// t.log("Log text")
+// t.Error("Error text")
+// t.Errorf("Error text %s", err)
+// t.Fatal("Fatal text")
+// t.Skip("Skip text")
+//}
