481f55b80908fbc9d1b4f253625ba68eeabec446
Rafael Hernamperez <rafinguer@gmail.com>
2022-10-05 19:27:13 +0200

    Added multiple variables

---
+++ LearnGoMultipleVar/multiplevar.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Definition of multiple variables
+func main() {
+	var (
+		planet   = "Venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Println("Planet: ", planet)
+	fmt.Println("Distance: ", distance)
+	fmt.Println("Orbital: ", orbital)
+	fmt.Println("Has life: ", hasLife)
+}
